2b55163a1d548eb140beceab648918df98957e6a
Vladimir Hidalgo <vladimiroski@gmail.com>
2016-10-06 03:24:23 -0600

    Login, paso 2/2

--- admingenerator/web/index.php
+++ admingenerator/web/index.php
@@ -16,8 +16,29 @@ if (1) {
 
 session_start();
 
+if (isset($_POST['usuario']) && isset($_POST['clave'])) {
+    require_once('../../configuracion.php');
+    require_once('../../SERV/PHP/db.php');
+
+    $usuario = db_codex($_POST['usuario']);
+    $clave = db_codex($_POST['clave']);
+    $c = "SELECT usuario, nivel FROM usuarios WHERE usuario = '{$usuario}' AND clave=SHA1('{$clave}') AND nivel IN ('gerente') AND disponible=1 LIMIT 1";
+    $r = db_consultar($c);
+
+    if ((int) db_num_resultados($r) > 0) {
+        $usuario = db_fetch($r);
+
+        $_SESSION['iniciado'] = true;
+        $_SESSION['usuario'] = $usuario;
+    } else {
+        $mensaje_login = "usuario o clave incorrectos";
+    }
+}
+
+
 if (!isset($_SESSION['iniciado'])) {
     require_once 'login.php';
     return;
 }
-require_once __DIR__.'/controllers/base.php';
+
+require_once __DIR__ . '/controllers/base.php';
